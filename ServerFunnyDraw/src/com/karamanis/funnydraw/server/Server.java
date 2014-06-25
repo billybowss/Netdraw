6980e694a76563a53e964199412542444a28d127
billybowss <billybowss@gmail.com>
2014-06-26 01:27:32 +0200

    Commit Release 1.0

--- ServerFunnyDraw/src/com/karamanis/funnydraw/server/Server.java
+++ ServerFunnyDraw/src/com/karamanis/funnydraw/server/Server.java
@@ -1,108 +1,161 @@
 package com.karamanis.funnydraw.server;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import org.apache.commons.codec.binary.Base64;
+import java.util.Random;
 
 public class Server {
-	private static DataInputStream dataInputStream1;
-	private static DataOutputStream dataOutputStream1;
-	private static DataInputStream dataInputStream2;
-	private static DataOutputStream dataOutputStream2;
 	private static Socket sock1 = null;
 	private static Socket sock2 = null;
 	private static ArrayList<String> listWords ;
 
 	public static void main(String[] args) throws IOException {
 	  // create socket
-		ServerSocket servsock = new ServerSocket(50100);
+		ServerSocket servsock = new ServerSocket(55555);
 		int players = 0;
-		boolean twoPlayers =false;
 		initializeListWords();
 	
 	  	while (true) {
-	
 		   System.out.println("Waiting...");
 		   if(players==0){
 			   sock1 = servsock.accept();
-			   dataInputStream1 = new DataInputStream(sock1.getInputStream());
-			   dataOutputStream1 = new DataOutputStream(sock1.getOutputStream());
+			  
 			   System.out.println("Player "+players+" - Accepted connection : " + sock1);
 			   players++;
 		   }else {
 			   sock2 = servsock.accept();
-			   dataInputStream2 = new DataInputStream(sock2.getInputStream());
-			   dataOutputStream2 = new DataOutputStream(sock2.getOutputStream());
+			   
+			   
 			   System.out.println("Player "+players+" - Accepted connection : " + sock2);
-			   twoPlayers = true;
-			   generateGame();
+			   generateGame(sock1,sock2);
 			   players = 0;
 		   }
-		   
-		   
-		  
-	   /*
-	   File myFile = new File("F:\\ic_launcher.png");
-	   System.out.println("Sending...");
-	   FileInputStream imageInFile = new FileInputStream(myFile);
-	   byte imageData[] = new byte[(int) myFile.length()];
-	   imageInFile.read(imageData);
-	
-	   /*
-	    * Converting Image byte array into Base64 String
-	    *
-		   String imageDataString = encodeImage(imageData);
-		   System.out.println(imageDataString.length());
-	
-		   System.out.println(imageDataString);
-		   dataOutputStream.writeUTF(imageDataString);
-	
-	*/
-	   		//dataOutputStream.write(0);
-		   //dataOutputStream.flush();
-		   //sock.close();
-		  }
-  
+		}
 	 }
-	public static void generateGame(){
+	
+	public static void generateGame(Socket p1, Socket p2) throws IOException{
+		System.out.println("Game Launched for two players");
+		final Socket player1 = p1;
+		final Socket player2 = p2;
+		DataInputStream dataInputStream1 = new DataInputStream(player1.getInputStream());
+		DataOutputStream dataOutputStream1 = new DataOutputStream(player1.getOutputStream());
+		DataInputStream dataInputStream2 = new DataInputStream(player2.getInputStream());
+		DataOutputStream dataOutputStream2 = new DataOutputStream(player2.getOutputStream());
+		
+		final PrintWriter oss1 = new PrintWriter( new BufferedWriter( new OutputStreamWriter(dataOutputStream1)));
+		final BufferedReader iss1 = new BufferedReader(new InputStreamReader(dataInputStream1));
+		
+		final PrintWriter oss2 = new PrintWriter( new BufferedWriter( new OutputStreamWriter(dataOutputStream2)));
+		final BufferedReader iss2 = new BufferedReader(new InputStreamReader(dataInputStream2));
+		
 		Thread t = new Thread(new Runnable() {
 			
 			@Override
-			public void run() {
-				System.out.println("Game Launched for two players");
+			public void run() {			
+				oss1.println("0");
+				oss1.flush();
+				System.out.println("-- Role Attribution P1 : CHECK !");
+				oss2.println("1");
+				oss2.flush();
+				System.out.println("-- Role Attribution P2 : CHECK !");
+				System.out.println("======================================");
+				Random rand = new Random();
+				int intRand = rand.nextInt(listWords.size());
+				String word = listWords.get(intRand);
+				System.out.println("-- Generate Word : CHECK !");
+				oss1.println(word);
+				oss1.flush();
+				System.out.println("-- Send Word : CHECK !");
+				
+				String image="";
+				
+				System.out.println("======================================");
+				
+				System.out.println("Waiting for Draw from Player 1...");
 				try {
-					dataOutputStream1.write(0);
-					dataOutputStream1.flush();	
-					dataOutputStream2.write(1);
-					dataOutputStream2.flush();	
+					image = iss1.readLine();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
+				System.out.println("-- Reception of the draw : CHECK !");
 				
+				oss2.println(image);
+				oss2.flush();
+				System.out.println("-- Send Draw : CHECK !");
+				System.out.println("======================================");
+				int cptProposition =1;
+				Boolean win = false;
+				while(cptProposition <4 && win==false){
+					System.out.println("Waiting for a proposition...");
+					String wordProposition="";
+					try {
+						wordProposition = iss2.readLine();
+						System.out.println("-- Player2 made a proposition ! #"+cptProposition+"       : "+wordProposition);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					if(word.compareToIgnoreCase(wordProposition)==0){
+						System.out.println("------ Word Matches !!");
+						oss1.println("WIN");
+						oss1.flush();
+						oss2.println("WIN");
+						oss2.flush();
+						win = true;
+					}else{
+						oss1.println("LOSE"+cptProposition);
+						oss1.flush();
+						oss2.println("LOSE"+cptProposition);
+						oss2.flush();
+						System.out.println("------ Word doesn't match !!");
+						System.out.println("======================================");
+						cptProposition++;
+					}
+				}
+				if(win){
+					try {
+						generateGame(player2, player1);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}else {
+					try {
+						generateGame(player1, player2);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		});
 		t.start();
 	}
-	 public static String encodeImage(byte[] imageByteArray) {
-	  return Base64.encodeBase64String(imageByteArray);
-	 }
+
 	private static void initializeListWords(){
 		listWords = new ArrayList<String>();
+		listWords.add("maison");
+		listWords.add("Chat");
+		listWords.add("chaise");
+		listWords.add("telephone");
+		listWords.add("ordinateur");
+		listWords.add("chaussette");
+		listWords.add("ballon");
+		listWords.add("voiture");
+		
+		/*
 		XmlPullParserFactory pullParserFactory;
 		try {
 			pullParserFactory = XmlPullParserFactory.newInstance();
 			XmlPullParser parser = pullParserFactory.newPullParser();
 
-			InputStream in_s = Server.class.getClassLoader().getResourceAsStream("assets/dictionnay.xml");
+			InputStream in_s = Server.class.getClassLoader().getResourceAsStream("dictionnay.xml");
 			if(in_s == null){
 				System.out.println("LE IN EST NULL");
 			}
@@ -115,9 +168,9 @@ public class Server {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
-	 private static void parseXML(XmlPullParser parser) throws XmlPullParserException,IOException
+	 /*private static void parseXML(XmlPullParser parser) throws XmlPullParserException,IOException
 		{
 		 	String word="";
 	        int eventType = parser.getEventType();
@@ -143,6 +196,6 @@ public class Server {
 	        }
 	        System.out.println("Fichier dictionnary.xml chargé !");
 	        System.out.println(listWords.size()+" mots ont été chargé ");      
-		}
+		}*/
 }
 
